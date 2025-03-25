e68ebec80ae208bbf878371d600bd83d38478289
Dante Dam <laggycomputer@yahoo.com>
2025-03-25 11:56:32 -0700

    skeletal Client::request internal

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// #![deny(missing_docs))
+
+use reqwest::RequestBuilder;
+use serde_json::Value;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("reqwest error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+    #[error("error deserializing json: {0}")]
+    Json(#[from] serde_json::Error),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub type Result<T> = std::result::Result<T, self::Error>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Clone, Debug)]
+pub struct Client {
+    http: reqwest::Client,
+    endpoint: String,
+    token: String,
+    user_agent: String,
+}
+
+impl Client {
+    async fn request<T>(&self, request: RequestBuilder) -> Result<Value>
+    where
+        T: for<'de> serde::de::Deserialize<'de>,
+    {
+        let built = request
+            .header("Authorization", format!("Bearer {}", self.token))
+            .header("User-Agent", &self.user_agent)
+            .build()?;
+
+        let response = self.http.execute(built).await?;
+
+        Ok(response.json().await?)
     }
 }
