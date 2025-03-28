1599dbc19a25dc3ce0625a4a99e1222ac6ae611b
Dante Dam <laggycomputer@yahoo.com>
2025-03-28 12:17:16 -0700

    add sort key option to get threads from course

--- src/opts.rs
+++ src/opts.rs
@@ -5,8 +5,28 @@ use serde::Deserialize;
 
 use serde::Serialize;
 
+/// How to sort responses as part of [`GetCourseThreadsOptions`].
+/// All unit variants are sort keys with known meaning.
+#[derive(Clone, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+pub enum GetCourseThreadsSortKey {
+    /// Newest threads first.
+    New,
+    /// Another sort key not known to this crate.
+    Other(String),
+}
+
+impl ToString for GetCourseThreadsSortKey {
+    fn to_string(&self) -> String {
+        match self {
+            Self::New => String::from("new"),
+            Self::Other(inner) => inner.clone(),
+        }
+    }
+}
+
 /// Options to [`crate::Client::get_course_threads`], centered on skip-take pagination.
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct GetCourseThreadsOptions {
     /// The limit on the number of threads to return. Values not greater than 100 appear to be
@@ -15,8 +35,8 @@ pub struct GetCourseThreadsOptions {
     /// The offset at which to begin; i.e. how many threads to skip before beginning to yield
     /// threads.
     pub offset: u64,
-    // not sure what this does
-    // pub sort: String,
+    /// A key by which to sort returned threads. See documentation for [`GetCourseThreadsSortKey`].
+    pub sort: GetCourseThreadsSortKey,
 }
 
 impl Default for GetCourseThreadsOptions {
@@ -24,17 +44,17 @@ impl Default for GetCourseThreadsOptions {
         Self {
             limit: 20,
             offset: 0,
-            // sort: String::from("new"),
+            sort: GetCourseThreadsSortKey::New,
         }
     }
 }
 
 impl GetCourseThreadsOptions {
-    pub(crate) fn as_params(&self) -> Vec<(&str, &impl Serialize)> {
+    pub(crate) fn as_params(&self) -> Vec<(&str, impl Serialize)> {
         vec![
-            ("limit", &self.limit),
-            ("offset", &self.offset),
-            // ("sort", &*self.sort),
+            ("limit", self.limit.to_string()),
+            ("offset", self.offset.to_string()),
+            ("sort", self.sort.to_string()),
         ]
     }
 }
